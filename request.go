0aadd2799973d4cf8751b7c8e6d40ae82c24a65e
TWT <TWT2333@outlook.com>
2022-02-16 23:52:38 +0800

    refactor: extract func requestJSON

--- request.go
+++ request.go
@@ -27,28 +27,39 @@ func setID(id string) {
 
 func doAction(jso interface{}) error {
 	u := url.URL{Scheme: "http", Host: *ADDR, Path: fmt.Sprintf("/games/%v/actions", gameID)}
-
 	jsb, err := json.Marshal(jso)
 	if err != nil {
 		return errors.Wrap(err, "marshal req")
 	}
-	req, err := http.NewRequest("POST", u.String(), bytes.NewBuffer(jsb))
+	rsp, err := requestJSON("POST", u.String(), bytes.NewBuffer(jsb))
 	if err != nil {
 		return errors.Wrap(err, "new req")
 	}
+
+	logrus.Infof("action(%s): %v", jsb, string(rsp))
+	return nil
+}
+
+func requestJSON(method string, url string, jso interface{}) (response string, err error) {
+	jsb, err := json.Marshal(jso)
+	if err != nil {
+		return "", errors.Wrap(err, "marshal req")
+	}
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(jsb))
+	if err != nil {
+		return "", err
+	}
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", authHeader)
-
 	rsp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return errors.Wrap(err, "post")
+		return "", err
 	}
 	defer rsp.Body.Close()
 
 	rspBytes, err := io.ReadAll(rsp.Body)
 	if err != nil {
-		return errors.Wrap(err, "read rsp")
+		return "", errors.Wrap(err, "read rsp")
 	}
-	logrus.Infof("request(%v): %v", string(rspBytes))
-	return nil
+	return string(rspBytes), nil
 }
